0920621b33a2ac519b5b662a8eec72b8931c11a2
jonatan villalva <jvillalva.sistemas@gmail.com>
2024-10-07 20:35:00 -0300

    iniciando proyecto. Ademas se comenzo con el desarrollo de el header del sitio.

--- misapuntesfront/src/App.jsx
+++ misapuntesfront/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
-
+import {BrowserRouter as Router , Route , Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
 function App() {
 
   return (
-    <>
-      <div>
-
-      </div>
-        
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
--- misapuntesfront/src/components/navbar/Navbar.jsx
+++ misapuntesfront/src/components/navbar/Navbar.jsx
@@ -1,7 +1,23 @@
-
+import { Link, Outlet } from "react-router-dom"
+import '../navbar/Navbar.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <header>
+      <Link to="/" className="linkDisabled">
+        <h2>Mis Apuntes <span>Front</span></h2>
+      </Link>
+      <nav>
+        <Link className="item" to="/">Inicio</Link>
+        <Link className="item" to="/about">About</Link>
+        <Link className="item" to="/apuntes">Apuntes</Link>
+        <Link className="item" to="/contactame">Contactame</Link>
+      </nav>
+    </header>
+    <main role="main">
+      <Outlet/>
+    </main>
+    </>
   )
 }
