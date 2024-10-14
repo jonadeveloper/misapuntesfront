62020d36249e5a7cc45e029d6d0c564c97c2460b
jonatan villalva <jvillalva.sistemas@gmail.com>
2024-10-13 22:44:34 -0300

    añadiendo imagen de prueba a la home y ordenando el contenido con estilos css

--- misapuntesfront/src/pages/home/Home.jsx
+++ misapuntesfront/src/pages/home/Home.jsx
@@ -1,15 +1,19 @@
 import './Home.css';
+import HomeImage from '../../assets/homeimage1.png'
 export default function Home() {
   return (
     <section id="home">
-      <div id='description'>
+      <div className='divhome'>
         <h1 className='title'>Tu Guía en el Universo del <br/> <span>Desarrollo Frontend.</span></h1>
-        <p>
-          Una iniciativa de <a href='https://instagram.com/jona.dev_ok' target="_blank">JonaDev</a>.
+        <p className='subtitle'>
+          Apuntes claros y prácticos que te ayudarán a enfrentar los desafíos más comunes en el desarrollo frontend moderno.
+        </p>
+        <p className="author">
+          Una iniciativa de <a href='https://instagram.com/jona.dev_ok' target="_blank">JonaDev</a> para la comunidad.
         </p>
       </div>
-      <div className="homeimage">
-        
+      <div className="divhome">
+        <img className='homeimage' src={HomeImage} alt="imagen descriptiva" />
       </div>
     </section>
   )
